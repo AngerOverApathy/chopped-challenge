4e79103f51a6c7b6b0f0370c36fbdb1c0680dc59
AngerOverApathy <violasian@gmail.com>
2023-05-19 21:40:44 -0700

    data variables created

--- script.js
+++ script.js
@@ -8,9 +8,14 @@ function getRecipe(){
         .then(data => {
 
             const mealArr = data.meals[0]
-            
-            console.log(mealArr.strMeal) //name
 
+            console.log(mealArr.strMeal) //name
+            console.log(mealsArr.strInstructions)//instructions
+
+            let foodImg = mealsArr.strMealThumb //food image
+            let vidTutorial = mealsArr.strYoutube //video tutorial
+
+            //get each ingredient from object properties
             Object.keys(mealArr).forEach(key => {
                 if (key.startsWith("strIngredient")) {
                   const ingredient = mealArr[key];
