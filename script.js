78b1412cf845e3173848bf8fc94ec28beeca0673
AngerOverApathy <violasian@gmail.com>
2023-05-19 22:19:16 -0700

    ingredient measurements added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let button = document.getElementById('btn')
 let recipeName = document.getElementById('recipe-name')
 let ingredientList  = document.getElementById('ingredients')
+const instructionContent = document.getElementById('instructions')
 
 button.addEventListener('click', getRecipe)
 
@@ -12,25 +13,46 @@ function getRecipe(){
             const mealArr = data.meals[0]
 
             recipeName.innerText = mealArr.strMeal //name
+
             let foodImg = mealArr.strMealThumb //food image
             console.log(foodImg)
-            console.log(mealArr.strInstructions)//instructions
-
-            let vidTutorial = mealArr.strYoutube //video tutorial
 
             //get each ingredient from object properties and append to list in DOM
+            // Object.keys(mealArr).forEach(key => {
+            //     if (key.startsWith("strIngredient")) {
+            //       const ingredient = mealArr[key];
+            //       if (ingredient && ingredient.trim() !== "") {
+
+            //         const li = document.createElement('li')
+            //         li.textContent = ingredient
+            //         ingredientList.appendChild(li)
+            //       }
+            //     }
+            //   })
+
+
             Object.keys(mealArr).forEach(key => {
                 if (key.startsWith("strIngredient")) {
-                  const ingredient = mealArr[key];
-                  if (ingredient && ingredient.trim() !== "") {
-                    console.log(ingredient);
-
+                  const ingredientNumber = key.slice(13); // Extract the ingredient number from the key
+                  const measurementKey = `strMeasure${ingredientNumber}`;
+              
+                  if (mealArr[key] && mealArr[measurementKey]) {
+                    const ingredient = mealArr[key];
+                    const measurement = mealArr[measurementKey];
+              
                     const li = document.createElement('li')
-                    li.textContent = ingredient
-                    ingredientList.appendChild(li)
+                    li.textContent = `${measurement} ${ingredient}`
+                    ingredientList.appendChild(li);
                   }
                 }
-              })
+              });
+
+
+            //instructions
+            const instructions = mealArr.strInstructions
+            instructionContent.textContent = instructions
+
+            //let vidTutorial = mealArr.strYoutube //video tutorial
 
         })
         .catch(err => console.error(err));
