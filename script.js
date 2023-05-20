90f839827e2936c6881986706a7fb08d7b02b616
AngerOverApathy <violasian@gmail.com>
2023-05-19 21:37:14 -0700

    foreach to pull ingredients created

--- script.js
+++ script.js
@@ -5,6 +5,21 @@ button.addEventListener('click', getRecipe)
 function getRecipe(){
     fetch('https://rocky-peak-03610.herokuapp.com/http://www.themealdb.com/api/json/v1/1/random.php')
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+
+            const mealArr = data.meals[0]
+            
+            console.log(mealArr.strMeal) //name
+
+            Object.keys(mealArr).forEach(key => {
+                if (key.startsWith("strIngredient")) {
+                  const ingredient = mealArr[key];
+                  if (ingredient && ingredient.trim() !== "") {
+                    console.log(ingredient);
+                  }
+                }
+              })
+
+        })
         .catch(err => console.error(err));
 }
